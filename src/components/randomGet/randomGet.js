3b874704a039a84c0867969fe143b3b9760dec2f
Baqytbayev <bakitbaev98@gmail.com>
2023-06-07 02:47:19 +0600

    Сделал поиск по имени и также стилизировал его

--- src/components/randomGet/randomGet.js
+++ src/components/randomGet/randomGet.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import './randomGet.css'
 import {useDispatch, useSelector} from "react-redux";
 import {getRandom} from "../../store/randomSlice";
@@ -8,6 +8,7 @@ import Random from "./random/random";
 const RandomGet = () => {
     const randoms = useSelector(state => state.random.random.results)
     console.log(randoms)
+    const [find, setFind] = useState('')
     const dispatch = useDispatch()
     useEffect(() => {
         dispatch(getRandom())
@@ -15,15 +16,29 @@ const RandomGet = () => {
     const handleRefresh = () => {
         dispatch(getRandom());
     };
+    const handleFind = (event) => {
+        setFind(event.target.value)
+    }
+    const findRandom = randoms.filter((random) => random.name.first.toLowerCase().includes(find.toLowerCase()))
     return (
-        <div>
-            <button onClick={handleRefresh}>Refresh</button>
-            {Object.keys(randoms).map(key => {
-                return <Random key={key}
-                               image={randoms[key].picture.large}
-                               text={randoms[key].name.first}/>
-
-            })}
+        <div className={'info'}>
+            <form onChange={handleFind}>
+                <input
+                    className={'input'}
+                    type={'text'}
+                    value={find}
+                    placeholder={'find by name'}
+                />
+                <button className={'findButton'}>Find</button>
+            </form>
+            <button className={'button'} onClick={handleRefresh}>Refresh</button>
+            {findRandom.map((random, i) => (
+                <Random
+                    key={i}
+                    image={random.picture.large}
+                    text={random.name.first}
+                />
+            ))}
         </div>
     )
 }
